e2603bab336f87b998c7d091a779dee5fc02b5bf
WhiskyTea <silntmind@gmail.com>
2023-06-14 12:52:02 -0700

    lost a file, then found it
    lost a file, then found it

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const app = express();
 app.set('view engine', 'pug');
 
 //get the project JSON
-const { data } = require('../data/flashcardData.json');
-const { projects } = data;
+// const { data } = require('data.json');
+// const { projects } = data;
 
 //set static access to the imgs,css, js
 app.use('/static', express.static('public'));
 
 //GET routes
 app.get('/', (req,res) =>{
-    app.render('index');
+    res.render('index');
 })
 
 app.get('/about', (req,res) =>{
-    app.render('about');
+    res.render('about');
 })
 
 app.get('/project/:id', (req,res) =>{
     const { id } = req.params; //what project is this?
-    const project = projects[id]; //snags the matching project from the JSON
-    app.render('project');
+    // const project = projects[id]; //snags the matching project from the JSON
+    res.render('project');
 })
 
 app.listen(3000, ()=>{
